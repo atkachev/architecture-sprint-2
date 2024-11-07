d0b301ec024629dc7869adf093eb50dacf5f9226
andreitkacev <tkachev87@gmail.com>
2024-11-07 20:33:53 +0400

    Второе задание

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -4,7 +4,7 @@
 # Инициализируем бд
 ###
 
-docker compose exec -T mongo-sharding_router mongosh --port 27020 <<EOF
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
 use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
